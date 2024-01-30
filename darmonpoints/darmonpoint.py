de6ccd4c5bf18133c8718735ec7d110eacd411bb
Marc Masdeu <marc.masdeu@gmail.com>
2024-01-30 18:37:10 +0100

    Removed lots of unneeded files

---
+++ darmonpoints/darmonpoint.py
@@ -0,0 +1,12 @@
+
+def darmon_point():
+    r'''
+    This is a placeholder for some documentation.
+
+    EXAMPLES::
+
+    sage: from darmonpoints.darmonpoint import darmon_point
+    sage: darmon_point()
+    42
+    '''
+    return 42
