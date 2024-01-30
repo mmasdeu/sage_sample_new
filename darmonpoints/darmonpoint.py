260744c1d9bb83de98984df08ac1c2027d65b307
Marc Masdeu <marc.masdeu@gmail.com>
2024-01-30 18:46:15 +0100

    Added docstring

--- darmonpoints/darmonpoint.py
+++ darmonpoints/darmonpoint.py
@@ -1,3 +1,16 @@
+r'''
+Darmon Points package
+=====================
+
+This file is the main file of the package.
+
+EXAMPLES::
+
+sage: from darmonpoints.darmonpoint import darmon_point
+sage: darmon_point()
+42
+
+'''
 
 def darmon_point():
     r'''
